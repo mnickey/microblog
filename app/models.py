4e19c6c197b4aae5954beb36f864d5289543016b
mnickey <mnickey@gmail.com>
2018-05-16 18:24:07 -0700

    added aux table for followers/followed users

--- app/models.py
+++ app/models.py
@@ -5,6 +5,11 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
+followers = db.Table(
+    db.Column('follower_id',db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id',db.Integer, db.ForeignKey('user.id'))
+)
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -27,6 +32,7 @@ class User(UserMixin, db.Model):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
             digest, size)
+
 
 @login.user_loader
 def load_user(id):

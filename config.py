079f2269cf09346108410e6de1cb51e319c4ec14
mnickey <mnickey@gmail.com>
2018-05-21 21:08:18 -0700

    added pagination links to user page

--- config.py
+++ config.py
@@ -14,4 +14,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['mnickey@mnickey.com']
-    POSTS_PER_PAGE = 3
+    POSTS_PER_PAGE = 25
